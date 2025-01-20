6e44cab3fbcc19506dba4b47b76656df00eed991
hnakashima <nakashima.alg57@gmail.com>
2025-01-20 15:31:17 +0900

    Refactor Variable methods to use Self for return types

--- src/variable.rs
+++ src/variable.rs
@@ -40,62 +40,62 @@ impl<'a> Variable<'a> {
     }
 
     #[inline]
-    pub fn sin(self) -> Variable<'a> {
+    pub fn sin(self) -> Self {
         self.apply_unary_function(f64::sin, f64::cos)
     }
 
     #[inline]
-    pub fn ln(self) -> Variable<'a> {
+    pub fn ln(self) -> Self {
         self.apply_unary_function(f64::ln, f64::recip)
     }
 
     #[inline]
-    pub fn powf(self, power: f64) -> Variable<'a> {
+    pub fn powf(self, power: f64) -> Self {
         self.apply_scalar_function(f64::powf, |x, power| power * x.powf(power - 1.0), power)
     }
 
     #[inline]
-    pub fn exp(self) -> Variable<'a> {
+    pub fn exp(self) -> Self {
         self.apply_unary_function(f64::exp, f64::exp)
     }
 
     #[inline]
-    pub fn sqrt(self) -> Variable<'a> {
+    pub fn sqrt(self) -> Self {
         self.apply_unary_function(f64::sqrt, |x| 0.5 * x.sqrt().recip())
     }
 
     #[inline]
-    pub fn cos(self) -> Variable<'a> {
+    pub fn cos(self) -> Self {
         self.apply_unary_function(f64::cos, |x| -f64::sin(x))
     }
 
     #[inline]
-    pub fn tan(self) -> Variable<'a> {
+    pub fn tan(self) -> Self {
         self.apply_unary_function(f64::tan, |x| f64::cos(x).powi(2).recip())
     }
 
     #[inline]
-    pub fn sinh(self) -> Variable<'a> {
+    pub fn sinh(self) -> Self {
         self.apply_unary_function(f64::sinh, f64::cosh)
     }
 
     #[inline]
-    pub fn cosh(self) -> Variable<'a> {
+    pub fn cosh(self) -> Self {
         self.apply_unary_function(f64::cosh, f64::sinh)
     }
 
     #[inline]
-    pub fn tanh(self) -> Variable<'a> {
+    pub fn tanh(self) -> Self {
         self.apply_unary_function(f64::tanh, |x| f64::cosh(x).powi(2).recip())
     }
 
     #[inline]
-    pub fn recip(self) -> Variable<'a> {
+    pub fn recip(self) -> Self {
         self.apply_unary_function(f64::recip, |x| -(x * x).recip())
     }
 
     #[inline(always)]
-    pub fn apply_unary_function(&self, f: UnaryFn<f64>, df: UnaryFn<f64>) -> Variable<'a> {
+    pub fn apply_unary_function(&self, f: UnaryFn<f64>, df: UnaryFn<f64>) -> Self {
         Variable {
             index: {
                 let operations = &mut self.tape.operations.borrow_mut();
@@ -114,7 +114,7 @@ impl<'a> Variable<'a> {
         f: BinaryFn<f64>,
         df: BinaryFn<f64>,
         scalar: f64,
-    ) -> Variable<'a> {
+    ) -> Self {
         Variable {
             index: {
                 let operations = &mut self.tape.operations.borrow_mut();
@@ -137,7 +137,7 @@ impl<'a> Variable<'a> {
         f: BinaryFn<f64>,
         dfdx: BinaryFn<f64>,
         dfdy: BinaryFn<f64>,
-    ) -> Variable<'a> {
+    ) -> Self {
         Variable {
             index: {
                 let operations = &mut self.tape.operations.borrow_mut();
@@ -171,7 +171,7 @@ impl Neg for Variable<'_> {
     }
 }
 
-impl<'a> Add<Variable<'a>> for Variable<'a> {
+impl<'a> Add<Self> for Variable<'a> {
     type Output = Self;
 
     #[inline]
@@ -189,7 +189,7 @@ impl<'a> Add<Variable<'a>> for Variable<'a> {
     }
 }
 
-impl<'a> Sub<Variable<'a>> for Variable<'a> {
+impl<'a> Sub<Self> for Variable<'a> {
     type Output = Self;
 
     #[inline]
@@ -207,7 +207,7 @@ impl<'a> Sub<Variable<'a>> for Variable<'a> {
     }
 }
 
-impl<'a> Mul<Variable<'a>> for Variable<'a> {
+impl<'a> Mul<Self> for Variable<'a> {
     type Output = Self;
 
     #[inline]
@@ -228,7 +228,7 @@ impl<'a> Mul<Variable<'a>> for Variable<'a> {
     }
 }
 
-impl<'a> Div<Variable<'a>> for Variable<'a> {
+impl<'a> Div<Self> for Variable<'a> {
     type Output = Self;
 
     #[inline]
@@ -241,7 +241,7 @@ impl<'a> Add<f64> for Variable<'a> {
     type Output = Variable<'a>;
 
     #[inline]
-    fn add(self, scalar: f64) -> Variable<'a> {
+    fn add(self, scalar: f64) -> Self::Output {
         Variable {
             index: {
                 let mut operations = self.tape.operations.borrow_mut();
@@ -259,7 +259,7 @@ impl<'a> Add<Variable<'a>> for f64 {
     type Output = Variable<'a>;
 
     #[inline]
-    fn add(self, var: Variable<'a>) -> Variable<'a> {
+    fn add(self, var: Self::Output) -> Self::Output {
         var + self
     }
 }
@@ -286,7 +286,7 @@ impl<'a> Sub<Variable<'a>> for f64 {
     type Output = Variable<'a>;
 
     #[inline]
-    fn sub(self, var: Variable<'a>) -> Variable<'a> {
+    fn sub(self, var: Self::Output) -> Self::Output {
         -var + self
     }
 }
@@ -295,7 +295,7 @@ impl<'a> Mul<f64> for Variable<'a> {
     type Output = Variable<'a>;
 
     #[inline]
-    fn mul(self, scalar: f64) -> Variable<'a> {
+    fn mul(self, scalar: f64) -> Self::Output {
         Variable {
             index: {
                 let mut operations = self.tape.operations.borrow_mut();
@@ -313,7 +313,7 @@ impl<'a> Mul<Variable<'a>> for f64 {
     type Output = Variable<'a>;
 
     #[inline]
-    fn mul(self, var: Variable<'a>) -> Variable<'a> {
+    fn mul(self, var: Self::Output) -> Self::Output {
         var * self
     }
 }
@@ -322,7 +322,7 @@ impl<'a> Div<f64> for Variable<'a> {
     type Output = Variable<'a>;
 
     #[inline]
-    fn div(self, scalar: f64) -> Variable<'a> {
+    fn div(self, scalar: f64) -> Self::Output {
         Variable {
             index: {
                 let operations = &mut self.tape.operations.borrow_mut();
@@ -341,7 +341,7 @@ impl<'a> Div<Variable<'a>> for f64 {
     type Output = Variable<'a>;
 
     #[inline]
-    fn div(self, var: Variable<'a>) -> Variable<'a> {
+    fn div(self, var: Self::Output) -> Self::Output {
         var.recip() * self
     }
 }
@@ -353,9 +353,9 @@ impl AddAssign<f64> for Variable<'_> {
     }
 }
 
-impl<'a> AddAssign<Variable<'a>> for Variable<'a> {
+impl<'a> AddAssign<Self> for Variable<'a> {
     #[inline]
-    fn add_assign(&mut self, other: Variable<'a>) {
+    fn add_assign(&mut self, other: Self) {
         *self = *self + other
     }
 }
@@ -367,9 +367,9 @@ impl SubAssign<f64> for Variable<'_> {
     }
 }
 
-impl<'a> SubAssign<Variable<'a>> for Variable<'a> {
+impl<'a> SubAssign<Self> for Variable<'a> {
     #[inline]
-    fn sub_assign(&mut self, other: Variable<'a>) {
+    fn sub_assign(&mut self, other: Self) {
         *self = *self - other
     }
 }
@@ -381,9 +381,9 @@ impl MulAssign<f64> for Variable<'_> {
     }
 }
 
-impl<'a> MulAssign<Variable<'a>> for Variable<'a> {
+impl<'a> MulAssign<Self> for Variable<'a> {
     #[inline]
-    fn mul_assign(&mut self, other: Variable<'a>) {
+    fn mul_assign(&mut self, other: Self) {
         *self = *self * other
     }
 }
@@ -395,9 +395,9 @@ impl DivAssign<f64> for Variable<'_> {
     }
 }
 
-impl<'a> DivAssign<Variable<'a>> for Variable<'a> {
+impl<'a> DivAssign<Self> for Variable<'a> {
     #[inline]
-    fn div_assign(&mut self, other: Variable<'a>) {
+    fn div_assign(&mut self, other: Self) {
         *self = *self / other
     }
 }
