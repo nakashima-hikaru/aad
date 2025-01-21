ec038c4848cd65a81b0b497728c5b0085cc2a763
hnakashima <nakashima.alg57@gmail.com>
2025-01-22 07:51:54 +0900

    Add new mathematical operations for `Variable`

--- src/overload/math.rs
+++ src/overload/math.rs
@@ -1,16 +1,16 @@
 use crate::variable::Variable;
 
+//
 impl Variable<'_> {
-    #[inline]
-    pub fn sin(self) -> Self {
-        self.apply_unary_function(f64::sin, f64::cos)
-    }
-
     #[inline]
     pub fn ln(self) -> Self {
         self.apply_unary_function(f64::ln, f64::recip)
     }
 
+    #[inline]
+    pub fn log(self, base: f64) -> Self {
+        self.apply_scalar_function(|x, b| x.log(b), |x, b| x.recip() / b.ln(), base)
+    }
     #[inline]
     pub fn powf(self, power: f64) -> Self {
         self.apply_scalar_function(f64::powf, |x, p| p * x.powf(p - 1.0), power)
@@ -29,6 +29,52 @@ impl Variable<'_> {
     #[inline]
     pub fn sqrt(self) -> Self {
         self.apply_unary_function(f64::sqrt, |x| 0.5f64 * x.sqrt().recip())
+    }
+
+    #[inline]
+    pub fn cbrt(self) -> Self {
+        self.apply_unary_function(f64::cbrt, |x| (1.0 / 3.0) * x.powf(-2.0 / 3.0))
+    }
+
+    #[inline]
+    pub fn recip(self) -> Self {
+        self.apply_unary_function(f64::recip, |x| -(x * x).recip())
+    }
+
+    #[inline]
+    pub fn exp2(self) -> Self {
+        self.apply_unary_function(f64::exp2, |x| f64::ln(2.0) * f64::exp2(x))
+    }
+
+    #[inline]
+    pub fn log2(self) -> Self {
+        self.apply_unary_function(f64::log2, |x| x.recip() * f64::ln(2.0).recip())
+    }
+
+    #[inline]
+    pub fn log10(self) -> Self {
+        self.apply_unary_function(f64::log10, |x| x.recip() * f64::ln(10.0).recip())
+    }
+
+    #[inline]
+    pub fn hypot(self, other: Self) -> Self {
+        self.apply_binary_function(&other, f64::hypot, |x, y| {
+            let denom = (x * x + y * y).sqrt();
+            (x / denom, y / denom)
+        })
+    }
+
+    #[inline]
+    pub fn abs(self) -> Self {
+        self.apply_unary_function(f64::abs, f64::signum)
+    }
+}
+
+// trigonometric functions
+impl Variable<'_> {
+    #[inline]
+    pub fn sin(self) -> Self {
+        self.apply_unary_function(f64::sin, f64::cos)
     }
 
     #[inline]
@@ -57,7 +103,32 @@ impl Variable<'_> {
     }
 
     #[inline]
-    pub fn recip(self) -> Self {
-        self.apply_unary_function(f64::recip, |x| -(x * x).recip())
+    pub fn asin(self) -> Self {
+        self.apply_unary_function(f64::asin, |x| (1.0 - x * x).sqrt().recip())
+    }
+
+    #[inline]
+    pub fn acos(self) -> Self {
+        self.apply_unary_function(f64::acos, |x| -(1.0 - x * x).sqrt().recip())
+    }
+
+    #[inline]
+    pub fn atan(self) -> Self {
+        self.apply_unary_function(f64::atan, |x| (1.0 + x * x).recip())
+    }
+
+    #[inline]
+    pub fn asinh(self) -> Self {
+        self.apply_unary_function(f64::asinh, |x| (x * x + 1.0).sqrt().recip())
+    }
+
+    #[inline]
+    pub fn acosh(self) -> Self {
+        self.apply_unary_function(f64::acosh, |x| (x * x - 1.0).sqrt().recip())
+    }
+
+    #[inline]
+    pub fn atanh(self) -> Self {
+        self.apply_unary_function(f64::atanh, |x| (1.0 - x * x).recip())
     }
 }
