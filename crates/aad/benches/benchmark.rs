6b595e1c096ed93c800134fc1830e11d9be68f33
hnakashima <nakashima.alg57@gmail.com>
2025-02-02 11:33:57 +0900

    Refactor benchmark tests for improved readability and performance

--- crates/aad/benches/benchmark.rs
+++ crates/aad/benches/benchmark.rs
@@ -5,15 +5,9 @@ use RustQuant_autodiff::{Accumulate, Graph};
 
 fn large_computation_graph_benchmark(c: &mut Criterion) {
     c.bench_function("large_computation_graph", |b| {
+        let tape = Tape::default();
+        let [x0, x1, x2, x3, x4] = tape.create_variables_as_array(&[1.0, 2.0, 3.0, 4.0, 5.0]);
         b.iter(|| {
-            let tape = Tape::default();
-
-            let x0 = tape.create_variable(1.0);
-            let x1 = tape.create_variable(2.0);
-            let x2 = tape.create_variable(3.0);
-            let x3 = tape.create_variable(4.0);
-            let x4 = tape.create_variable(5.0);
-
             let mut result = x0;
             for i in 0..100_000 {
                 result += (((result + x1) * x2.sin()) + (x3 * x4.ln())) * (x2 + f64::from(i).ln());
@@ -27,17 +21,16 @@ fn large_computation_graph_benchmark(c: &mut Criterion) {
 
 fn large_computation_graph_benchmark_rust_quant(c: &mut Criterion) {
     c.bench_function("large_computation_graph_rust_quant", |b| {
+        let tape = Graph::default();
+
+        let x0 = tape.var(1.0);
+        let x1 = tape.var(2.0);
+        let x2 = tape.var(3.0);
+        let x3 = tape.var(4.0);
+        let x4 = tape.var(5.0);
         b.iter(|| {
-            let tape = Graph::default();
-
-            let x0 = tape.var(1.0);
-            let x1 = tape.var(2.0);
-            let x2 = tape.var(3.0);
-            let x3 = tape.var(4.0);
-            let x4 = tape.var(5.0);
-
             let mut result = x0;
-            for i in 0..100000 {
+            for i in 0..100_000 {
                 result += (((result + x1) * x2.sin()) + (x3 * x4.ln())) * (x2 + f64::from(i).ln());
             }
             black_box(result);
@@ -57,7 +50,7 @@ fn large_computation_graph_benchmark_f64(c: &mut Criterion) {
             let x4 = 5.0_f64;
 
             let mut result = x0;
-            for i in 0..10000 {
+            for i in 0..100_000 {
                 result += (((result + x1) * x2.sin()) + (x3 * x4.ln())) * (x2 + f64::from(i).ln());
                 black_box(result);
             }
