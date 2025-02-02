bd22b8f72dbcfbbd46b90845d6aef9ae4cb6d392
hnakashima <nakashima.alg57@gmail.com>
2025-02-02 12:22:43 +0900

    Refactor gradient retrieval methods for improved type safety and performance

--- crates/aad/src/gradients.rs
+++ crates/aad/src/gradients.rs
@@ -5,12 +5,21 @@ impl<F: Copy> Gradients<F>
 where
     Vec<F>: FromIterator<F>,
 {
+    #[inline]
     #[must_use]
     pub fn get_gradient(&self, x: &Variable<F>) -> F {
         self.0[x.index]
     }
+
+    #[inline]
     #[must_use]
-    pub fn get_gradients(&self, vars: &[Variable<F>]) -> Vec<F> {
+    pub fn get_gradients<const N: usize>(&self, vars: &[Variable<F>; N]) -> [F; N] {
+        std::array::from_fn(|i| self.get_gradient(&vars[i]))
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn get_gradients_vec(&self, vars: &[Variable<F>]) -> Vec<F> {
         vars.iter().map(|var| self.get_gradient(var)).collect()
     }
 }
