4ebd72bd9ae1e5cade1826fae4ef67464cbf8ca9
hnakashima <nakashima.alg57@gmail.com>
2025-02-22 17:02:29 +0900

    Generalize math methods for nested Variables using FloatLike trait
    Refactor math methods in the AAD library to:
    - Use generic FloatLike trait for nested Variable implementations
    - Remove duplicate method implementations
    - Simplify code by leveraging trait methods
    - Improve type flexibility for mathematical operations

--- crates/aad/src/overload/math.rs
+++ crates/aad/src/overload/math.rs
@@ -1,9 +1,5 @@
 use crate::{FloatLike, variable::Variable};
-use num_traits::One;
-use std::{
-    cmp::Ordering,
-    ops::{Div as _, Mul, Neg, Sub as _},
-};
+use std::cmp::Ordering;
 
 impl From<Variable<'_, f64>> for f64 {
     fn from(value: Variable<'_, f64>) -> Self {
@@ -14,167 +10,6 @@ impl From<Variable<'_, f64>> for f64 {
 impl From<Variable<'_, Variable<'_, f64>>> for f64 {
     fn from(value: Variable<'_, Variable<'_, f64>>) -> Self {
         value.value.value
-    }
-}
-
-impl Variable<'_, f64> {
-    #[inline]
-    #[must_use]
-    pub fn sin(self) -> Self {
-        self.apply_unary_function(f64::sin, f64::cos)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn cos(self) -> Self {
-        self.apply_unary_function(f64::cos, |x| -x.sin())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn tan(self) -> Self {
-        self.apply_unary_function(f64::tan, |x| x.cos().powi(2).recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn ln(self) -> Self {
-        self.apply_unary_function(f64::ln, f64::recip)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn log(self, base: f64) -> Self {
-        self.apply_scalar_function(f64::log, |x, b| x.recip().mul(b.ln().recip()), base)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn powf(self, power: f64) -> Self {
-        self.apply_scalar_function(f64::powf, |x, p| p.mul(x.powf(p.sub(f64::one()))), power)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn powi(self, power: i32) -> Self {
-        self.apply_scalar_function(f64::powi, |x, p| f64::from(p) * x.powi(p - 1), power)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn exp(self) -> Self {
-        self.apply_unary_function(f64::exp, f64::exp)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn sqrt(self) -> Self {
-        self.apply_unary_function(f64::sqrt, |x| x.sqrt().recip().div(f64::one() + f64::one()))
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn cbrt(self) -> Self {
-        self.apply_unary_function(f64::cbrt, |x| {
-            x.powf(-(f64::from(2) / f64::from(3))) / f64::from(3)
-        })
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn recip(self) -> Self {
-        self.apply_unary_function(f64::recip, |x| x.powi(2).recip().neg())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn exp2(self) -> Self {
-        self.apply_unary_function(f64::exp2, |x| {
-            f64::ln(f64::one() + f64::one()) * f64::exp2(x)
-        })
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn log2(self) -> Self {
-        self.apply_unary_function(f64::log2, |x| {
-            x.recip() * f64::ln(f64::one() + f64::one()).recip()
-        })
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn log10(self) -> Self {
-        self.apply_unary_function(f64::log10, |x| x.recip() * 10.0_f64.ln().recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn hypot(self, other: Self) -> Self {
-        self.apply_binary_function(&other, f64::hypot, |x, y| {
-            let denom = x.hypot(y);
-            (x / denom, y / denom)
-        })
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn abs(self) -> Self {
-        self.apply_unary_function(f64::abs, f64::signum)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn sinh(self) -> Self {
-        self.apply_unary_function(f64::sinh, f64::cosh)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn cosh(self) -> Self {
-        self.apply_unary_function(f64::cosh, f64::sinh)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn tanh(self) -> Self {
-        self.apply_unary_function(f64::tanh, |x| f64::cosh(x).powi(2).recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn asin(self) -> Self {
-        self.apply_unary_function(f64::asin, |x| (f64::one() - x * x).sqrt().recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn acos(self) -> Self {
-        self.apply_unary_function(f64::acos, |x| -(f64::one() - x * x).sqrt().recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn atan(self) -> Self {
-        self.apply_unary_function(f64::atan, |x| (f64::one() + x * x).recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn asinh(self) -> Self {
-        self.apply_unary_function(f64::asinh, |x| (x * x + f64::one()).sqrt().recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn acosh(self) -> Self {
-        self.apply_unary_function(f64::acosh, |x| (x * x - f64::one()).sqrt().recip())
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn atanh(self) -> Self {
-        self.apply_unary_function(f64::atanh, |x| (f64::one() - x * x).recip())
     }
 }
 
@@ -305,79 +140,65 @@ impl FloatLike<f64> for Variable<'_, f64> {
     }
 }
 
-impl Variable<'_, Variable<'_, f64>> {
+impl<F: FloatLike<f64>> Variable<'_, F> {
     #[inline]
     #[must_use]
     pub fn sin(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::sin, Variable::<'_, f64>::cos)
+        self.apply_unary_function(F::sin, F::cos)
     }
 
     #[inline]
     #[must_use]
     pub fn cos(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::cos, |x| -x.sin())
+        self.apply_unary_function(F::cos, |x| -x.sin())
     }
 
     #[inline]
     #[must_use]
     pub fn tan(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::tan, |x| x.cos().powi(2).recip())
+        self.apply_unary_function(F::tan, |x| x.cos().powi(2).recip())
     }
 
     #[inline]
     #[must_use]
     pub fn ln(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::ln, Variable::<'_, f64>::recip)
+        self.apply_unary_function(F::ln, F::recip)
     }
 
     #[inline]
     #[must_use]
     pub fn log(self, base: f64) -> Self {
-        self.apply_scalar_function(
-            Variable::<'_, f64>::log,
-            |x, b| x.recip().mul(b.ln().recip()),
-            base,
-        )
+        self.apply_scalar_function(F::log, |x, b| x.recip().mul(b.ln().recip()), base)
     }
 
     #[inline]
     #[must_use]
     pub fn powf(self, power: f64) -> Self {
-        self.apply_scalar_function(
-            Variable::<'_, f64>::powf,
-            |x, p| p.mul(x.powf(p.sub(f64::one()))),
-            power,
-        )
+        self.apply_scalar_function(F::powf, |x, p| x.powf(p - 1.0).mul(p), power)
     }
 
     #[inline]
     #[must_use]
     pub fn powi(self, power: i32) -> Self {
-        self.apply_scalar_function(
-            Variable::<'_, f64>::powi,
-            |x, p| f64::from(p) * x.powi(p - 1),
-            power,
-        )
+        self.apply_scalar_function(F::powi, |x, p| F::from(f64::from(p)) * x.powi(p - 1), power)
     }
 
     #[inline]
     #[must_use]
     pub fn exp(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::exp, Variable::<'_, f64>::exp)
+        self.apply_unary_function(F::exp, F::exp)
     }
 
     #[inline]
     #[must_use]
     pub fn sqrt(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::sqrt, |x| {
-            x.sqrt().recip().div(f64::one() + f64::one())
-        })
+        self.apply_unary_function(F::sqrt, |x| x.sqrt().recip().div(F::one() + F::one()))
     }
 
     #[inline]
     #[must_use]
     pub fn cbrt(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::cbrt, |x| {
+        self.apply_unary_function(F::cbrt, |x| {
             x.powf(-(f64::from(2) / f64::from(3))) / f64::from(3)
         })
     }
@@ -385,40 +206,31 @@ impl Variable<'_, Variable<'_, f64>> {
     #[inline]
     #[must_use]
     pub fn recip(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::recip, |x| x.powi(2).recip().neg())
+        self.apply_unary_function(F::recip, |x| x.powi(2).recip().neg())
     }
 
     #[inline]
     #[must_use]
     pub fn exp2(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::exp2, |x| {
-            Variable::<'_, f64>::ln(Variable::<'_, f64>::one() + Variable::<'_, f64>::one())
-                * Variable::<'_, f64>::exp2(x)
-        })
+        self.apply_unary_function(F::exp2, |x| F::ln(F::one() + F::one()) * F::exp2(x))
     }
 
     #[inline]
     #[must_use]
     pub fn log2(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::log2, |x| {
-            x.recip()
-                * Variable::<'_, f64>::ln(Variable::<'_, f64>::one() + Variable::<'_, f64>::one())
-                    .recip()
-        })
+        self.apply_unary_function(F::log2, |x| x.recip() * F::ln(F::one() + F::one()).recip())
     }
 
     #[inline]
     #[must_use]
     pub fn log10(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::log10, |x| {
-            x.recip() * 10.0_f64.ln().recip()
-        })
+        self.apply_unary_function(F::log10, |x| x.recip() * 10.0_f64.ln().recip())
     }
 
     #[inline]
     #[must_use]
     pub fn hypot(self, other: Self) -> Self {
-        self.apply_binary_function(&other, Variable::<'_, f64>::hypot, |x, y| {
+        self.apply_binary_function(&other, F::hypot, |x, y| {
             let denom = x.hypot(y);
             (x / denom, y / denom)
         })
@@ -434,69 +246,55 @@ impl Variable<'_, Variable<'_, f64>> {
     #[inline]
     #[must_use]
     pub fn sinh(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::sinh, Variable::<'_, f64>::cosh)
+        self.apply_unary_function(F::sinh, F::cosh)
     }
 
     #[inline]
     #[must_use]
     pub fn cosh(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::cosh, Variable::<'_, f64>::sinh)
+        self.apply_unary_function(F::cosh, F::sinh)
     }
 
     #[inline]
     #[must_use]
     pub fn tanh(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::tanh, |x| {
-            Variable::<'_, f64>::cosh(x).powi(2).recip()
-        })
+        self.apply_unary_function(F::tanh, |x| x.cosh().powi(2).recip())
     }
 
     #[inline]
     #[must_use]
     pub fn asin(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::asin, |x| {
-            (Variable::<'_, f64>::one() - x * x).sqrt().recip()
-        })
+        self.apply_unary_function(F::asin, |x| (F::one() - x * x).sqrt().recip())
     }
 
     #[inline]
     #[must_use]
     pub fn acos(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::acos, |x| {
-            -(Variable::<'_, f64>::one() - x * x).sqrt().recip()
-        })
+        self.apply_unary_function(F::acos, |x| -(F::one() - x * x).sqrt().recip())
     }
 
     #[inline]
     #[must_use]
     pub fn atan(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::atan, |x| {
-            (Variable::<'_, f64>::one() + x * x).recip()
-        })
+        self.apply_unary_function(F::atan, |x| (F::one() + x * x).recip())
     }
 
     #[inline]
     #[must_use]
     pub fn asinh(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::asinh, |x| {
-            (x * x + Variable::<'_, f64>::one()).sqrt().recip()
-        })
+        self.apply_unary_function(F::asinh, |x| (x * x + F::one()).sqrt().recip())
     }
 
     #[inline]
     #[must_use]
     pub fn acosh(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::acosh, |x| {
-            (x * x - Variable::<'_, f64>::one()).sqrt().recip()
-        })
+        self.apply_unary_function(F::acosh, |x| (x * x - F::one()).sqrt().recip())
     }
 
     #[inline]
     #[must_use]
     pub fn atanh(self) -> Self {
-        self.apply_unary_function(Variable::<'_, f64>::atanh, |x| {
-            (Variable::<'_, f64>::one() - x * x).recip()
-        })
+        self.apply_unary_function(F::atanh, |x| (F::one() - x * x).recip())
     }
 }
 
